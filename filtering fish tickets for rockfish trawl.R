a3ed7cbdd22259200bc9d9a4898418625d4d1da3
Blake Feist <blake.feist@noaa.gov>
2026-09-23 13:49:59 -0700

    Updated filters

--- filtering fish tickets for rockfish trawl.R
+++ filtering fish tickets for rockfish trawl.R
@@ -10,6 +10,11 @@ library(DescTools)
 # load master 1994 - 2023 fish tickets file (used for the paper)
 fishtickets <- readRDS("~/Documents/GitHub/VMS-pipeline/Confidential/raw_data/fish_tickets/all_fishtickets_1994_2023.rds")
 
+# load master 2011 - 2025 fish tickets file (to add in 2024 data)
+fishtickets <- readRDS("~/Documents/Projects/Ecosystem Science/Whale Entanglement/West Coast Take Reduction Team (TRT)/CONFIDENTIAL fish tickets from PacFIN/all_fishtix_2011to2025.rds")
+
+
+
 # filter for bottom trawl gear only
 fishtickets_trawl <- fishtickets %>%
   filter(
@@ -23,12 +28,31 @@ fishtickets_trawl <- fishtickets %>%
   )
 
 # filter for rockfish only
-trawl_rock <- fishtickets_trawl %>%
-  filter(COMPLEX == "ROCK")
+# trawl_rock <- fishtickets_trawl %>%
+#   filter(COMPLEX == "ROCK")
+
+# filter for 7 rockfish species in RCA paper
+seven_spp <- 
+  filter(fishtickets_trawl,
+         PACFIN_SPECIES_COMMON_NAME == 'BOCACCIO' |
+           PACFIN_SPECIES_COMMON_NAME == 'NOM. BOCACCIO' |
+           PACFIN_SPECIES_COMMON_NAME == 'CANARY ROCKFISH' |
+           PACFIN_SPECIES_COMMON_NAME == 'NOM. CANARY ROCKFISH' |
+           PACFIN_SPECIES_COMMON_NAME == 'COWCOD ROCKFISH' |
+           PACFIN_SPECIES_COMMON_NAME == 'NOM. COWCOD ROCKFISH' |
+           PACFIN_SPECIES_COMMON_NAME == 'DARKBLOTCHED ROCKFISH' |
+           PACFIN_SPECIES_COMMON_NAME == 'NOM. DARKBLOTCHED ROCKFISH' |
+           PACFIN_SPECIES_COMMON_NAME == 'PACIFIC OCEAN PERCH' |
+           PACFIN_SPECIES_COMMON_NAME == 'NOM. POP' |
+           PACFIN_SPECIES_COMMON_NAME == 'WIDOW ROCKFISH' |
+           PACFIN_SPECIES_COMMON_NAME == 'NOM. WIDOW ROCKFISH' |
+           PACFIN_SPECIES_COMMON_NAME == 'YELLOWEYE ROCKFISH' |
+           PACFIN_SPECIES_COMMON_NAME == 'NOM. YELLOWEYE ROCKFISH'
+  )
 
 
 # filter for fish tix 2002 - 2023
-rockfish_tix <- trawl_rock %>%
+seven_spp <- seven_spp %>%
   filter(LANDING_YEAR > 2001)
 
 # save a tab delimited file
